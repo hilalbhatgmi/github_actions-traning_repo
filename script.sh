f64d3b58c565821f7be2aa1fe1e050ff6dc7a8f2
hilalbhatgmi <102292797+hilalbhatgmi@users.noreply.github.com>
2022-08-02 12:30:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+echo "Hi from `hostname`  at `date`..!" >> demo.txt
+sleep 5
+echo "Hi from `hostname`  at `date`..!" >> demo.txt
+sleep 10
+echo "Hi from `hostname`  at `date`..!" >> demo.txt
